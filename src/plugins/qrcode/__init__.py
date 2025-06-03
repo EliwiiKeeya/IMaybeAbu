f71f0d9a21118586671d08d98d6d8468a3987bdf
Eliwii_Keeya <Eliwii_Keeya@163.com>
2025-06-03 13:09:21 +0000

    refactor(QRCodeAbu): format code to comply with PEP8 and remove redundant elements

--- src/plugins/qrcode/__init__.py
+++ src/plugins/qrcode/__init__.py
@@ -1,5 +1,4 @@
 ﻿from nonebot.adapters.discord import MessageSegment
-
 from nonebot.adapters.discord.api import (
     StringOption,
     File,
@@ -15,13 +14,14 @@ matcher = on_slash_command(
     name="qrcode",
     description="二維碼",
     options=[
-        StringOption(name="text", description="内容",required=True)
+        StringOption(name="text", description="内容", required=True)
     ],
 )
+
 
 @matcher.handle()
 async def handle_qrcode(text: CommandOption[str]):
     await matcher.send_deferred_response()
     file = QRCodeAbu.excute(text)
-    file = File(content=file, filename="qrcode.png")    
+    file = File(content=file, filename="qrcode.png")
     await matcher.send_followup_msg(MessageSegment.attachment(file))
